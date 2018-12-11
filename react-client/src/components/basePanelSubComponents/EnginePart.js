d85a2dbf931518ce29ead03238dd767aebd5366c
Jieling Yang <j.yang@pilz.ie>
2018-12-11 17:55:31 +0000

    moved engine svg images to component

--- react-client/src/components/basePanelSubComponents/EnginePart.js
+++ react-client/src/components/basePanelSubComponents/EnginePart.js
@@ -2,21 +2,18 @@ import React from "react";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 import StyleObject from "../../classes/StyleObject";
-import {TRANSITION_TIME_SLOW} from "../../utilities/CONSTANTS_TIME";
-import {COMMON_TYPE, SVG_IMAGE_NAME} from "../../utilities/CONSTANTS_STRING";
-import {BLUR_LEVEL, ENGINE_PART_INDICES} from "../../utilities/CONSTANTS_NUMBER";
-import {getEngineMiddleFaceSvg, getEngineFrontFaceSvg, getEngineBackFaceSvg} from "../../utilities/svgIcons";
+import {COMMON_TYPE} from "../../utilities/CONSTANTS_STRING";
 import EnginePartMenu from "./EnginePartMenu";
 import BaseModelWithStateAndShape from "../../classes/BaseModelWithStateAndShape";
 import type {engineReducerType} from "../../reducers/engineReducer";
-import {LEVEL2_CONSOLE_FONT, LEVEL2_CONSOLE_PREFIX, LEVEL3_CONSOLE_FONT, LEVEL3_CONSOLE_PREFIX} from "../../utilities/CONSTANTS_CONSOLE_FONT";
+import {LEVEL2_CONSOLE_FONT, LEVEL2_CONSOLE_PREFIX} from "../../utilities/CONSTANTS_CONSOLE_FONT";
 import EnginePartActionComponent from "./EnginePartActionComponent";
+import EnginePartSvg from "./EnginePartSvg";
 
 type EnginePartPropsType = {
     /* Values from parent */
     enginePartIndex: number,
     /* Values from mapStateToProps() */
-    engineBasicColor: string,
     engineState: engineReducerType
 }
 
@@ -33,12 +30,6 @@ const EnginePart = (props: EnginePartPropsType) =>
     let shouldFocus: boolean = isThisEnginePartSelected || mouseHoverOnThisEnginePart || (!mouseHoverOnAnyEnginePart && !isAnyEnginePartSelected);
     let actualEngineRotationX: number = props.engineState.engineInitialRotationX + props.engineState.engineRotationX;
     let actualEngineRotationY: number = props.engineState.engineInitialRotationY + props.engineState.engineRotationY;
-    let enginePartBlur: BLUR_LEVEL = shouldFocus
-                                     ? BLUR_LEVEL.NONE
-                                     : BLUR_LEVEL.LIGHT;
-    let enginePartOpacity: number = shouldFocus
-                                    ? 1
-                                    : 0.4;
 
     let enginePartContainerDivStyleObject: StyleObject = new StyleObject(COMMON_TYPE.DEFAULT)
         .setBasics(enginePartModel.getWidth(), enginePartModel.getHeight(), enginePartModel.getX(), enginePartModel.getY())
@@ -51,8 +42,12 @@ const EnginePart = (props: EnginePartPropsType) =>
 
     console.log(LEVEL2_CONSOLE_PREFIX + enginePartStringId, LEVEL2_CONSOLE_FONT);
     return <div id={enginePartStringId} style={enginePartContainerDivStyleObject.getStyle()}>
-        {/* This getEnginePartFaces() is not a functional component by itself because of browser compatibility problem, firefox won't render them properly if they are wrapped as one element */}
-        {getEnginePartFaces(enginePartStringId, props.enginePartIndex, enginePartModel.getWidth(), mouseHoverOnThisEnginePart, isThisEnginePartSelected, enginePartBlur, enginePartOpacity, props.engineBasicColor)}
+        <EnginePartSvg enginePartStringId={enginePartStringId}
+                       enginePartIndex={props.enginePartIndex}
+                       enginePartSize={enginePartModel.getWidth()}
+                       isThisEnginePartSelected={isThisEnginePartSelected}
+                       mouseHoverOnThisEnginePart={mouseHoverOnThisEnginePart}
+                       shouldFocus={shouldFocus}/>
         <EnginePartActionComponent enginePartStringId={enginePartStringId}
                                    enginePartIndex={props.enginePartIndex}
                                    disableMouse={isAnyEnginePartSelected && !isThisEnginePartSelected}/>
@@ -67,91 +62,9 @@ const EnginePart = (props: EnginePartPropsType) =>
     </div>;
 };
 
-const getEnginePartFaces: Array = (enginePartStringId: string, enginePartIndex: number, enginePartSize: number, mouseHoverOnThisEnginePart: boolean, isThisEnginePartSelected: boolean, enginePartBlurLevel: BLUR_LEVEL, enginePartOpacity: number, engineBasicColor: string) =>
-{
-    let isFrontPart: boolean = enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_FRONT;
-    let isMiddlePart: boolean = enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_MIDDLE;
-    let isBackPart: boolean = enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_BACK;
-    let initialTranslationZ: number = 0;
-    let initialRotationZ: number = 0;
-    let individualRotationZ: number = 0;
-    let selectTranslationZ: number = 0;
-    let hoverTranslationZ: number = 0;
-    let svgFaces: Array = isFrontPart
-                          ? getEngineFrontFaceSvg(engineBasicColor)
-                          : isMiddlePart
-                            ? getEngineMiddleFaceSvg(engineBasicColor)
-                            : isBackPart
-                              ? getEngineBackFaceSvg(engineBasicColor)
-                              : [];
-    let numberOfSvg: number = svgFaces.length;
-    let svgGap: number = enginePartSize / (numberOfSvg + 1);
-
-    if (isFrontPart)
-    {
-        initialTranslationZ = isThisEnginePartSelected
-                              ? 0
-                              : -enginePartSize / 2;
-        initialRotationZ = 180;
-        hoverTranslationZ = svgGap / 2;
-        selectTranslationZ = svgGap;
-    }
-    else if (isMiddlePart)
-    {
-        initialRotationZ = 0;
-        hoverTranslationZ = 0;
-        selectTranslationZ = svgGap / 2;
-    }
-    else if (isBackPart)
-    {
-        initialTranslationZ = enginePartSize / 2;
-        hoverTranslationZ = -svgGap / 2;
-        selectTranslationZ = -svgGap;
-    }
-
-    console.log(LEVEL3_CONSOLE_PREFIX + enginePartStringId + SVG_IMAGE_NAME, LEVEL3_CONSOLE_FONT);
-    return svgFaces.map((svg, i) =>
-    {
-        let indexVariant: number = i + 1;
-        if (isMiddlePart)
-        {
-            indexVariant = i - (numberOfSvg - 1) / 2;
-            initialTranslationZ = svgGap * indexVariant;
-            individualRotationZ = 180 * (i - 1);
-        }
-        let transitionTranslationZ: number = initialTranslationZ + (isThisEnginePartSelected
-                                                                    ? selectTranslationZ * indexVariant
-                                                                    : mouseHoverOnThisEnginePart
-                                                                      ? hoverTranslationZ * indexVariant
-                                                                      : 0);
-        let transitionRotationZ: number = initialRotationZ + (isThisEnginePartSelected
-                                                              ? 270
-                                                              : mouseHoverOnThisEnginePart
-                                                                ? 270
-                                                                : individualRotationZ);
-        let transitionRotationY: number = isThisEnginePartSelected
-                                          ? 0
-                                          : -90;
-
-        let enginePartFaceDivStyleObject: StyleObject = new StyleObject(COMMON_TYPE.DEFAULT)
-            .setBasics("100%", "100%", 0, 0)
-            .addRotationY(transitionRotationY)
-            .setBlur(enginePartBlurLevel)
-            .setOpacity(enginePartOpacity)
-            .addRotationZ(transitionRotationZ)
-            .addTranslationZ(transitionTranslationZ)
-            .addTransition("filter", TRANSITION_TIME_SLOW)
-            .addTransition("opacity", TRANSITION_TIME_SLOW)
-            .addTransition("transform", TRANSITION_TIME_SLOW);
-
-        return <div key={i} style={enginePartFaceDivStyleObject.getStyle()}>{svg}</div>
-    });
-};
-
 const mapStateToProps = (store) =>
 {
     return {
-        engineBasicColor: store.appState.engineBasicColor,
         engineState: store.engineState,
     };
 };
---
+++ react-client/src/components/basePanelSubComponents/EnginePartSvg.js
@@ -0,0 +1,122 @@
+import React from "react";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import {COMMON_TYPE, SVG_IMAGE_NAME} from "../../utilities/CONSTANTS_STRING";
+import StyleObject from "../../classes/StyleObject";
+import {LEVEL3_CONSOLE_FONT, LEVEL3_CONSOLE_PREFIX} from "../../utilities/CONSTANTS_CONSOLE_FONT";
+import {BLUR_LEVEL, ENGINE_PART_INDICES} from "../../utilities/CONSTANTS_NUMBER";
+import {getEngineBackFaceSvg, getEngineFrontFaceSvg, getEngineMiddleFaceSvg} from "../../utilities/svgIcons";
+import {TRANSITION_TIME_SLOW} from "../../utilities/CONSTANTS_TIME";
+
+type EnginePartSvgPropsType = {
+    /* Values from parent */
+    enginePartStringId: string,
+    enginePartIndex: number,
+    enginePartSize: number,
+    isThisEnginePartSelected: boolean,
+    mouseHoverOnThisEnginePart: boolean,
+    shouldFocus: boolean,
+    /* Values from mapStateToProps() */
+    engineBasicColor: string,
+}
+
+const EnginePartSvg = (props: EnginePartSvgPropsType) =>
+{
+    let isFrontPart: boolean = props.enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_FRONT;
+    let isMiddlePart: boolean = props.enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_MIDDLE;
+    let isBackPart: boolean = props.enginePartIndex === ENGINE_PART_INDICES.ENGINE_PART_BACK;
+    let initialTranslationZ: number = 0;
+    let initialRotationZ: number = 0;
+    let individualRotationZ: number = 0;
+    let selectTranslationZ: number = 0;
+    let hoverTranslationZ: number = 0;
+    let enginePartBlur: BLUR_LEVEL = props.shouldFocus
+                                     ? BLUR_LEVEL.NONE
+                                     : BLUR_LEVEL.LIGHT;
+    let enginePartOpacity: number = props.shouldFocus
+                                    ? 1
+                                    : 0.4;
+    let svgFaces: Array = isFrontPart
+                          ? getEngineFrontFaceSvg(props.engineBasicColor)
+                          : isMiddlePart
+                            ? getEngineMiddleFaceSvg(props.engineBasicColor)
+                            : isBackPart
+                              ? getEngineBackFaceSvg(props.engineBasicColor)
+                              : [];
+    let numberOfSvg: number = svgFaces.length;
+    let svgGap: number = props.enginePartSize / (numberOfSvg + 1);
+
+    if (isFrontPart)
+    {
+        initialTranslationZ = props.isThisEnginePartSelected
+                              ? 0
+                              : -props.enginePartSize / 2;
+        initialRotationZ = 180;
+        hoverTranslationZ = svgGap / 2;
+        selectTranslationZ = svgGap;
+    }
+    else if (isMiddlePart)
+    {
+        initialRotationZ = 0;
+        hoverTranslationZ = 0;
+        selectTranslationZ = svgGap / 2;
+    }
+    else if (isBackPart)
+    {
+        initialTranslationZ = props.enginePartSize / 2;
+        hoverTranslationZ = -svgGap / 2;
+        selectTranslationZ = -svgGap;
+    }
+
+    return svgFaces.map((svg, i) =>
+    {
+        let indexVariant: number = i + 1;
+        if (isMiddlePart)
+        {
+            indexVariant = i - (numberOfSvg - 1) / 2;
+            initialTranslationZ = svgGap * indexVariant;
+            individualRotationZ = 180 * (i - 1);
+        }
+        let transitionTranslationZ: number = initialTranslationZ + (props.isThisEnginePartSelected
+                                                                    ? selectTranslationZ * indexVariant
+                                                                    : props.mouseHoverOnThisEnginePart
+                                                                      ? hoverTranslationZ * indexVariant
+                                                                      : 0);
+        let transitionRotationZ: number = initialRotationZ + (props.isThisEnginePartSelected
+                                                              ? 270
+                                                              : props.mouseHoverOnThisEnginePart
+                                                                ? 270
+                                                                : individualRotationZ);
+        let transitionRotationY: number = props.isThisEnginePartSelected
+                                          ? 0
+                                          : -90;
+
+        let enginePartFaceDivStyleObject: StyleObject = new StyleObject(COMMON_TYPE.DEFAULT)
+            .setBasics("100%", "100%", 0, 0)
+            .addRotationY(transitionRotationY)
+            .setBlur(enginePartBlur)
+            .setOpacity(enginePartOpacity)
+            .addRotationZ(transitionRotationZ)
+            .addTranslationZ(transitionTranslationZ)
+            .addTransition("filter", TRANSITION_TIME_SLOW)
+            .addTransition("opacity", TRANSITION_TIME_SLOW)
+            .addTransition("transform", TRANSITION_TIME_SLOW);
+
+        console.log(LEVEL3_CONSOLE_PREFIX + props.enginePartStringId + SVG_IMAGE_NAME + i, LEVEL3_CONSOLE_FONT);
+        return <div key={i} style={enginePartFaceDivStyleObject.getStyle()}>{svg}</div>
+    });
+};
+
+const mapStateToProps = (store) =>
+{
+    return {
+        engineBasicColor: store.appState.engineBasicColor,
+    };
+};
+
+const matchDispatchToProps = (dispatch) =>
+{
+    return bindActionCreators({}, dispatch);
+};
+
+export default connect(mapStateToProps, matchDispatchToProps)(EnginePartSvg);
